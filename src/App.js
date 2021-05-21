6a10e0a26bbd9a59b1ae69469c90ddb0e909abcc
Yash Vardhan <yashvardhan513@gmail.com>
2021-05-21 17:56:12 +0530

    add the contact component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { ButtonComponent } from 'webiu-npm'
+import {ContactComponent} from 'webiu-npm'
 
 function App() {
   return (
     <div>
       <ButtonComponent heading="Button Component" buttonText="Button" />
+      <ContactComponent contactMessage="Contact us" subscribeMessage="Subscribe to our news" />
     </div>
   );
 }
